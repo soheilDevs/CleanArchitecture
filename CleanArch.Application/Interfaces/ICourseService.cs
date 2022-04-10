b37e30a857eb6c5f63745aa3417f5441856b3d8c
sohei <sohei@DESKTOP-RLU32GJ>
2022-04-10 21:51:42 +0430

    Initial Application Layer

---
+++ CleanArch.Application/Interfaces/ICourseService.cs
@@ -0,0 +1,9 @@
+﻿using CleanArch.Application.ViewModels;
+
+namespace CleanArch.Application.Services
+{
+    public interface ICourseService
+    {
+        CourseViewModel GetCourses();
+    }
+}
---
+++ CleanArch.Domains/Interfaces/ICourseRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace CleanArch.Domains.Interfaces
+{
+    public interface ICourseRepository
+    {
+        IEnumerable<Course> GeCourses();
+    }
+}
