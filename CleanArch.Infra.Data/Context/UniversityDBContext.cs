e1316e5e9e27632910b05531b5d24e268ffc36fb
sohei <sohei@DESKTOP-RLU32GJ>
2022-04-10 20:23:19 +0430

    Create Db and Nuget

---
+++ CleanArch.Infra.Data/Context/UniversityDBContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArch.Domains;
+using Microsoft.EntityFrameworkCore;
+
+namespace CleanArch.Infra.Data.Context
+{
+    public class UniversityDBContext : DbContext
+    {
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options):base(options)
+        {
+
+        }
+
+       
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
