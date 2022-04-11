3b5d3df28f222fd2ccba9f937660103ad7164952
sohei <sohei@DESKTOP-RLU32GJ>
2022-04-11 13:06:32 +0430

    Create View

---
+++ CleanArch.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+
+namespace CleanArch.Mvc.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+        public IActionResult Index()
+        {
+            var model = _courseService.GetCourses();
+            return View(model);
+        }
+    }
+}
